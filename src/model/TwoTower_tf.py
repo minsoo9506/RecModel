ce0fef5b84db67d5819ab0dfe6aa15345715f52c
minsoo9506 <ghktjd15gh@gmail.com>
2023-11-12 23:18:11 +0900

    feat: Two Tower model tf 구현

--- src/model/TwoTower_tf.py
+++ src/model/TwoTower_tf.py
@@ -34,7 +34,7 @@ class RepresentLayer(tf.keras.layers.Layer):
         for x in inputs:
             vocab = int_vocabs[x.name]
             lookup = tf.keras.layers.IntegerLookup(vocabulary=vocab)(x)
-            embedding = tf.keras.layers.Embedding(vocab.size, 8)(lookup)
+            embedding = tf.keras.layers.Embedding(vocab.size + 1, 8)(lookup)
             outputs.append(embedding)
         concat = tf.keras.layers.Concatenate()(outputs)
         return tf.keras.Model(inputs=inputs, outputs=concat)
@@ -50,7 +50,7 @@ class RepresentLayer(tf.keras.layers.Layer):
         for x in inputs:
             vocab = str_vocabs[x.name]
             lookup = tf.keras.layers.StringLookup(vocabulary=vocab)(x)
-            embedding = tf.keras.layers.Embedding(vocab.size, 8)(lookup)
+            embedding = tf.keras.layers.Embedding(vocab.size + 1, 8)(lookup)
             outputs.append(embedding)
         concat = tf.keras.layers.Concatenate()(outputs)
         return tf.keras.Model(inputs=inputs, outputs=concat)
@@ -87,8 +87,77 @@ class RepresentLayer(tf.keras.layers.Layer):
 
 
 class TowerModel(tf.keras.Model):
-    pass
+    def __init__(
+        self,
+        layer_size_list: list[int],
+        int_vocabs: dict[str, np.ndarray],
+        str_vocabs: dict[str, np.ndarray],
+        statistics: dict[str, tuple[float, float]],
+    ):
+        super().__init__()
+        self.embedding_layer = RepresentLayer(int_vocabs, str_vocabs, statistics)
+
+        self.dense_layer = tf.keras.Sequential()
+        for layer_size in layer_size_list[:-1]:
+            self.dense_layer.add(tf.keras.layers.Dense(layer_size, activation="relu"))
+        self.dense_layer.add(tf.keras.layers.Dense(layer_size_list[-1]))
+
+    def call(self, inputs):
+        feature_embedding = self.embedding_layer(inputs)
+        out = self.dense_layer(feature_embedding)
+        return out
 
 
-class TwoTower(tf.keras.Model):
-    pass
+class TwoTower(tfrs.models.Model):
+    def __init__(
+        self,
+        user_int_vocabs: dict[str, np.ndarray],
+        user_str_vocabs: dict[str, np.ndarray],
+        user_statistics: dict[str, tuple[float, float]],
+        user_features: list[str],
+        user_dense_layer_size_list: list[int],
+        item_int_vocabs: dict[str, np.ndarray],
+        item_str_vocabs: dict[str, np.ndarray],
+        item_statistics: dict[str, tuple[float, float]],
+        item_features: list[str],
+        item_dense_layer_size_list: list[int],
+        tf_dataset_item,
+    ):
+        super().__init__()
+        self.query_model = TowerModel(
+            user_dense_layer_size_list,
+            user_int_vocabs,
+            user_str_vocabs,
+            user_statistics,
+        )
+        self.candidate_model = TowerModel(
+            item_dense_layer_size_list,
+            item_int_vocabs,
+            item_str_vocabs,
+            item_statistics,
+        )
+        self.user_features = user_features
+        self.item_features = item_features
+
+        self.task = tfrs.tasks.Retrieval(
+            loss=tf.keras.losses.CategoricalCrossentropy(
+                reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE
+            ),
+            metrics=tfrs.metrics.FactorizedTopK(
+                candidates=tf_dataset_item.batch(128).map(self.candidate_model),
+            ),
+        )
+
+    def compute_loss(self, features, training=False):
+        query_embedding = self.query_model(
+            {key: features[key] for key in features.keys() if key in self.user_features}
+        )
+
+        candidate_embedding = self.candidate_model(
+            {key: features[key] for key in features.keys() if key in self.item_features}
+        )
+
+        out = self.task(
+            query_embedding, candidate_embedding, compute_metrics=not training
+        )
+        return out
